83aab4d5ed84d843f3533fd20ed5435a35674541
Hass2an <hassankhan00914@gmail.com>
2024-03-01 10:18:54 +0500

    refresh & access token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import  Jwt  from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -41,11 +43,47 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:[true, "Password us required"],
     },
-    
+
     refreshToken:{
         type:String,
     }
 
 },{timestamps:true});
+
+userSchema.pre("save", function async(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    Jwt.sign(
+        {
+            _id:this._id,
+            username : this.username,
+            email: this.email,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    Jwt.sign({
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
 
 const User = mongoose.model("User", userSchema)
